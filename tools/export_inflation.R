419c35d349a339a3de2064c8e8f20d6f5a8c4743
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2020-08-26 11:51:50 +0200

    draft working version

---
+++ tools/export_inflation.R
@@ -0,0 +1,12 @@
+library(eurostat)
+library(tidyr)
+library(lubridate)
+library(readr)
+
+get_eurostat('prc_hicp_aind') %>% 
+  filter(geo == "EU", coicop == "CP00") %>% 
+  mutate(year = year(time), values = values / 100) %>% 
+  select(unit, year, values) %>% 
+  pivot_wider(names_from = unit, values_from = values) %>% 
+  rename(index = INX_A_AVG, rate = RCH_A_AVG) %>% 
+  write("data/inflation.csv")
---
+++ tools/extract_cover.R
@@ -0,0 +1,14 @@
+library(fs)
+library(pdftools)
+library(dplyr)
+library(png)
+
+
+base_dir <- "C:/Users/spi/Downloads"
+filename <- "standard-input-for-eurocontrol-cost-benefit-analyses-2018-edition-8-version-2.6.pdf"
+
+pdf_file <- fs::path_abs(filename, start = base_dir)
+# fs::file_exists(pdf_file)
+
+pdf_render_page(pdf_file, page = 1) %>% 
+  png::writePNG(here::here("cover.png"))
