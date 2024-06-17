039b588dca82c2b2f85d5f68b0921b23af5ac1a1
BURDASH Olga <olga.burdash@eurocontrol.int>
2024-06-17 17:21:14 +0200

    release 10

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf ./_book/
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_book')"
+rm -rf ./_book/*
+rm -fR ./_freeze/*
+quarto render index.qmd --to html
